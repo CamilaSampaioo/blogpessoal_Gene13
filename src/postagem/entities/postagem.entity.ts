fdd8f09cdfe789440bdfb52768eeaae4e377cecf
Camila Sampaio <camila.sampaiodev@gmail.com>
2026-03-04 09:55:38 -0300

    🐛 FIX: Validação de tamanho da string: titulo e texto.

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,5 +1,5 @@
 import { Transform, TransformFnParams } from "class-transformer";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: "tb_postagens"}) // CREATE TABLE tb_postagens
@@ -15,8 +15,9 @@ export class Postagem{
 
   @Transform(({ value }: TransformFnParams) => value?.trim()) // Remover espaços em branco I/F
   @IsNotEmpty() // Forçar digitação
+  @Length(10, 1000, {message: " O Texto deve ter entre 10 a 1000 caracteres"})
   @Column({length: 1000, nullable: false}) // VARCHAR(1000) NOT NULL
-  texto: string;
+  Text: string;
 
   @UpdateDateColumn() // Atualiza a data na criação e na atualização
   data: Date;
